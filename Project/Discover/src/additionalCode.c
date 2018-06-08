255db2722ca510dcd5d67ce9a1b2112adb451995
Mike Hewitt <matroxmike@icloud.com>
2018-06-08 13:30:56 +1000

    source code mods for visual test

---
+++ Project/Discover/src/additionalCode.c
@@ -0,0 +1,7 @@
+//  7/6/2018 23:57
+// for github test   
+// repo created on github
+GPIO_WriteHigh(GPIOD, GPIO_PIN_2);        // mike added 
+      // 27/12/2017 upload check
+      
+      
---
+++ Project/Discover/src/mike.c
@@ -0,0 +1,7 @@
+//  
+    
+
+GPIO_WriteHigh(GPIOD, GPIO_PIN_2);        // mike added 
+      // 27/12/2017 upload check
+      
+      
